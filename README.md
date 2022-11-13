57033105d29997ca41c0aa64a50e201d1cd3f064
ChetasP <chetaspatil.7@gmail.com>
2022-11-13 21:20:08 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # dsml_demo
  This is a git demo project for scaler
+ This is just for testing
---
+++ main.py
@@ -0,0 +1,7 @@
+def calculateSUM(a,b):
+    return a+b
+
+
+if __name__=="__main__":
+    s = calculateSUM(5,4)
+    print(s)
